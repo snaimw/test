a77e0b7f45bd8a3c9518efb0c75bf5d94f297302
Aleksey Razbakov <aleksey@razbakov.com>
2014-02-11 10:18:43 +0100

    changed format

--- index.php
+++ index.php
@@ -5,12 +5,11 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<?php
-    echo 'Gregory Usenko';
-    echo "Denis Kryachko";
-	echo 'Dmitriy Chuiko';
-	echo 'Alexander Vasilyev';
-	echo 'Melgosh Yury';
-?>
+<ul>
+    <li>Gregory Usenko</li>
+    <li>Denis Kryachko</li>
+    <li>Dmitriy Chuiko</li>
+    <li>Alexander Vasilyev</li>
+    <li>Melgosh Yury</li>
 </body>
 </html>
